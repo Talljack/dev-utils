5b95cec79dcfa7d7fc711ff932056131986232ac
Yugang Cao <1442770751@qq.com>
2023-11-12 10:28:21 +0800

    feat: menu (#1)

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,14 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+mod menu;
+
+use menu::build_menu;
+
 fn main() {
+  let context = tauri::generate_context!();
   tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    .menu(build_menu(&context))
+    .run(context)
+    .expect("error while running dev-utils app");
 }
---
+++ src-tauri/src/menu.rs
@@ -0,0 +1,52 @@
+use tauri::{utils::assets::EmbeddedAssets, Context, Menu};
+
+pub fn build_menu(context: &Context<EmbeddedAssets>) -> Menu {
+  // get the app name from the tauri.conf.json package
+  let app_name = &context.package_info().name;
+  println!("app_name: {}", app_name);
+  #[cfg(target_os = "macos")]
+  use tauri::{MenuItem, Submenu, AboutMetadata};
+  {
+    // 应用主菜单
+    let app_menu = Submenu::new(
+      "",
+      // MenuItem::About 为原生菜单
+      Menu::new().add_native_item(MenuItem::About(app_name.into(), AboutMetadata::new()))
+      .add_native_item(MenuItem::Separator)
+      .add_native_item(MenuItem::Services)
+      .add_native_item(MenuItem::Separator)
+      .add_native_item(MenuItem::Hide)
+      .add_native_item(MenuItem::HideOthers)
+      .add_native_item(MenuItem::ShowAll)
+      .add_native_item(MenuItem::Separator)
+      .add_native_item(MenuItem::Quit)
+    );
+    let operation_menu = Submenu::new(
+"Edit",
+Menu::new()
+      .add_native_item(MenuItem::Undo)
+      .add_native_item(MenuItem::Redo)
+      .add_native_item(MenuItem::Separator)
+      .add_native_item(MenuItem::Cut)
+      .add_native_item(MenuItem::Copy)
+      .add_native_item(MenuItem::Paste)
+      .add_native_item(MenuItem::Separator)
+      .add_native_item(MenuItem::SelectAll)
+    );
+    Menu::new()
+      .add_submenu(app_menu)
+      .add_submenu(operation_menu)
+  }
+  // {
+  //   use tauri::SystemTrayMenu;
+
+  //   menu = menu.system_tray(vec![SystemTrayMenu::Item(
+  //     tauri::SystemTrayMenuItem::Custom("Quit".to_string(), "quit".to_string()),
+  //   )]);
+  // }
+  #[cfg(not(target_os = "macos"))]
+  {
+    Menu::new()
+  }
+
+}
