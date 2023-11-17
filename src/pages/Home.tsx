5e00d7aefda874af71eaa282e0da8e78a48fe032
Yugang Cao <1442770751@qq.com>
2023-11-17 09:37:20 +0800

    fix: router

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,33 @@
-import React from 'react'
+import LeftPanel from '@/components/LeftPanel'
+import { getUtilItem } from '@/utils'
+import React, { useEffect } from 'react'
+import { Outlet, useLocation, useNavigate, useParams } from 'react-router-dom'
 
 const Home = () => {
-  return <div>Home</div>
+  const { id } = useParams()
+  const navigate = useNavigate()
+  const location = useLocation()
+  useEffect(() => {
+    const pathName = location.pathname
+    const [, path, paramId] = pathName.split('/')
+    if (!path) {
+      navigate('/utils/json-formatter')
+      return
+    } else if (!paramId) {
+      const utilItem = getUtilItem(paramId)
+      utilItem.paramId &&
+        navigate(`/utils/${utilItem.paramId}`, { replace: true })
+      return
+    }
+  }, [id, location.pathname, navigate])
+  return (
+    <div className="flex w-full">
+      <LeftPanel />
+      <main className="flex-1 bg-red-100">
+        <Outlet />
+      </main>
+    </div>
+  )
 }
 
 export default Home
