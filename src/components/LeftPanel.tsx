d4aaf20afab31e4661ec5b01f05025a23ce2f196
Yugang Cao <1442770751@qq.com>
2023-12-03 15:12:15 +0800

    feat: search

--- src/components/LeftPanel.tsx
+++ src/components/LeftPanel.tsx
@@ -1,20 +1,36 @@
 import { Input } from '@/components/ui/input'
 import { utilMap } from '@/constant'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link, useParams } from 'react-router-dom'
 
 const LeftPanel = () => {
   const { id } = useParams()
+  const [search, setSearch] = useState('')
+  const [filteredUtils, setFilteredUtils] = useState(utilMap)
+  useEffect(() => {
+    if (search) {
+      const filtered = utilMap.filter(item =>
+        item.name.toLowerCase().includes(search.toLowerCase())
+      )
+      setFilteredUtils(filtered)
+    } else {
+      setFilteredUtils(utilMap)
+    }
+  }, [search])
   const getActiveClass = (pathId: string) => {
     return id === pathId ? 'bg-sky-500 text-white' : ''
   }
   return (
-    <div className="flex w-[400px] flex-col px-2 py-4">
+    <div className="flex w-[300px] flex-col px-2 py-4">
       <div className="top flex w-full items-center justify-between gap-4 border-b px-2 py-4">
-        <Input placeholder="Search" />
+        <Input
+          placeholder="Search"
+          value={search}
+          onChange={e => setSearch(e.target.value)}
+        />
       </div>
       <div className="bottom flex-1 py-4">
-        {utilMap.map(utilItem => (
+        {filteredUtils.map(utilItem => (
           <Link
             key={utilItem.name}
             className={`group mt-2 flex cursor-pointer items-center space-y-3 rounded-md px-8 py-2 first-of-type:mt-0 hover:bg-sky-500 ${getActiveClass(
