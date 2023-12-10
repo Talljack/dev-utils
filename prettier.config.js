6283b26233477a7055cb3c41deb76d2b2feb42b5
Talljack <Talljack@users.noreply.github.com>
2023-12-10 10:04:54 +0000

    deploy: 5a28df84a587a08035fa7d53dd326ab965256028

--- prettier.config.js
+++
@@ -1,8 +0,0 @@
-export default {
-  semi: false,
-  printWidth: 80,
-  singleQuote: true,
-  trailingComma: 'none',
-  arrowParens: 'avoid',
-  plugins: ['prettier-plugin-organize-imports', 'prettier-plugin-tailwindcss']
-}
--- src/type.ts
+++
@@ -1,7 +0,0 @@
-import type { ReactNode } from 'react'
-export type UtilItem = {
-  name: string
-  path: string
-  icon?: ReactNode
-  paramId: string
-}
