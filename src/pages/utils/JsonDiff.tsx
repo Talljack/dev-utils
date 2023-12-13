2879e3cd5bbaece3d3b42889bccc9b58217561d9
yugang.cao <yugang.cao@tusen.ai>
2023-12-13 19:03:22 +0800

    fix: optimization

--- src/pages/utils/JsonDiff.tsx
+++ src/pages/utils/JsonDiff.tsx
@@ -5,53 +5,63 @@ import { MonacoDiffEditor } from 'monaco-editor-component/react'
 import type { FC } from 'react'
 import React, { useState } from 'react'
 
+type FormatJsonType = 'original' | 'modified'
+
 const JsonDiff: FC = () => {
   const [originalValue, setOriginalValue] = useState('')
   const [modifiedValue, setModifiedValue] = useState('')
   const [showDiff, setShowDiff] = useState(false)
-  const formatJson = (json: string) => {
+  const [originValueResult, setOriginalValueResult] = useState('')
+  const [modifiedValueResult, setModifiedValueResult] = useState('')
+  const formatJson = (json: string, type: FormatJsonType = 'original') => {
     try {
       return JSON.stringify(JSON.parse(json), null, 2)
     } catch (error) {
-      return ''
+      if (type === 'original') {
+        setOriginalValueResult('Invalid JSON')
+      } else {
+        setModifiedValueResult('Invalid JSON')
+      }
+      return json
     }
   }
   return (
-    <div className="flex h-full flex-col">
+    <div className="flex flex-col h-full">
       <div className="flex h-full">
         {showDiff ? (
-          <div className="flex h-full w-full flex-col items-center justify-center gap-6">
+          <div className="flex flex-col items-center justify-center w-full h-full gap-6">
             <MonacoDiffEditor
               options={{
                 readOnly: true,
                 fontSize: 16
               }}
               language="json"
-              originalValue={formatJson(originalValue)}
-              value={formatJson(modifiedValue)}
+              originalValue={originalValue}
+              value={modifiedValue}
             />
-            <Button className="h-10 w-20" onClick={() => setShowDiff(false)}>
+            <Button className="w-20 h-10" onClick={() => setShowDiff(false)}>
               Back
             </Button>
           </div>
         ) : (
           <div
-            className="input flex h-full w-full flex-1 flex-col justify-center gap-6"
+            className="flex flex-col justify-center flex-1 w-full h-full gap-6 input"
             style={{
               alignItems: 'center'
             }}
           >
-            <div className="flex h-full w-full flex-1 justify-between gap-4">
+            <div className="flex justify-between flex-1 w-full h-full gap-4">
               <CodeEditor
                 code={originalValue}
                 onChange={(value: string) => {
-                  setOriginalValue(value)
+                  setOriginalValue(formatJson(value, 'original'))
                 }}
                 options={{
                   lineDecorationsWidth: 0,
                   lineNumbersMinChars: 0,
                   fontSize: 16
                 }}
+                inputResult={originValueResult}
                 language="json"
                 sampleValue='{"name":"John","age":30,"city":"New York"}'
                 inputLabel="Original:"
@@ -59,19 +69,20 @@ const JsonDiff: FC = () => {
               <CodeEditor
                 code={modifiedValue}
                 onChange={(value: string) => {
-                  setModifiedValue(value)
+                  setModifiedValue(formatJson(value, 'modified'))
                 }}
                 options={{
                   lineDecorationsWidth: 0,
                   lineNumbersMinChars: 0,
                   fontSize: 16
                 }}
+                inputResult={modifiedValueResult}
                 language="json"
                 sampleValue='{"name":"Tom","age":32,"city":"New York"}'
                 inputLabel="Modified:"
               />
             </div>
-            <Button className="h-10 w-20" onClick={() => setShowDiff(true)}>
+            <Button className="w-20 h-10" onClick={() => setShowDiff(true)}>
               Diff
             </Button>
           </div>
