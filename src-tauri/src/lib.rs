efd0a2bb77cad3825865d07f52a9a8562f4c2e4d
Yugang Cao <1442770751@qq.com>
2023-12-17 13:09:23 +0800

    feat: menu tauri

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,13 +1,10 @@
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 mod menu;
-mod tray;
+// mod tray;
 // mod command;
-
+// use tauri::menu::{Menu, MenuItem, PredefinedMenuItem, Submenu};
 use menu::build_menu;
 // use tray::{build_tray_menu, handler_tray_menu};
-// use command::read_markdown_file;
-use tauri_plugin_dialog::DialogExt;
-
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
@@ -17,21 +14,14 @@ fn greet(name: &str) -> String {
 pub fn run() {
   let context = tauri::generate_context!();
   tauri::Builder::default()
+    .enable_macos_default_menu(false)
     .plugin(tauri_plugin_shell::init())
     .plugin(tauri_plugin_dialog::init())
     .plugin(tauri_plugin_clipboard_manager::init())
     .plugin(tauri_plugin_fs::init())
-    .setup(move |app| {
-      let _ = build_menu(app);
-      app.dialog().file().pick_file(|file_path| {
-          // do something with the optional file path here
-          // the file path is `None` if the user closed the dialog
-          println!("File path: {:?}", file_path)
-      });
-      app.dialog().message("Tauri is Awesome!").show(|show| {
-        println!("Dialog was {}.", if show { "shown" } else { "hidden" });
-      });
-      Ok(())
+    // .menu(|handle| Ok(build_menu(handle).expect("failed to build menu")))
+    .menu(|app| {
+      Ok(build_menu(app).expect("failed to set up menu"))
     })
     // .system_tray(build_tray_menu())
     // .on_system_tray_event(handler_tray_menu)
--- src/components/LeftPanel.tsx
+++ src/components/LeftPanel.tsx
@@ -1,7 +1,8 @@
 import { Input } from '@/components/ui/input'
 import { utilMap } from '@/constant'
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Link, useParams } from 'react-router-dom'
+import { ScrollArea } from "@/components/ui/scroll-area"
 
 const LeftPanel = () => {
   const { id } = useParams()
@@ -22,28 +23,30 @@ const LeftPanel = () => {
   }
   return (
     <div className="flex shrink-0 basis-[300px] flex-col px-2 py-4">
-      <div className="top flex w-full items-center justify-between gap-4 border-b px-2 py-4">
-        <Input
-          placeholder="Search"
-          value={search}
-          onChange={e => setSearch(e.target.value)}
-        />
-      </div>
-      <div className="bottom flex-1 py-4">
-        {filteredUtils.map(utilItem => (
-          <Link
-            key={utilItem.name}
-            className={`group mt-2 flex cursor-pointer items-center space-y-3 rounded-md px-8 py-2 first-of-type:mt-0 hover:bg-sky-500 ${getActiveClass(
-              utilItem.paramId
-            )}`}
-            to={utilItem.path}
-          >
-            <div className="relative ml-2 flex items-center group-hover:text-white">
-              {utilItem.name}
-            </div>
-          </Link>
-        ))}
-      </div>
+        <div className="flex items-center justify-between w-full gap-4 px-2 py-4 border-b top">
+          <Input
+            placeholder="Search"
+            value={search}
+            onChange={e => setSearch(e.target.value)}
+          />
+        </div>
+        <ScrollArea className="flex-1">
+          <div className="flex-1 py-4 bottom">
+            {filteredUtils.map(utilItem => (
+              <Link
+                key={utilItem.name}
+                className={`group mt-2 flex cursor-pointer items-center space-y-3 rounded-md px-8 py-2 first-of-type:mt-0 hover:bg-sky-500 ${getActiveClass(
+                  utilItem.paramId
+                )}`}
+                to={utilItem.path}
+              >
+                <div className="relative flex items-center ml-2 group-hover:text-white">
+                  {utilItem.name}
+                </div>
+              </Link>
+            ))}
+          </div>
+        </ScrollArea>
     </div>
   )
 }
