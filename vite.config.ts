76a09e7db585d2c0ce8f5eebf639cda7cb36599f
Yugang Cao <1442770751@qq.com>
2023-12-28 20:34:14 +0800

    chore: update viteconfig

--- vite.config.ts
+++ vite.config.ts
@@ -1,37 +1,34 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
-import svgr from 'vite-plugin-svgr'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import viteTsconfigPaths from 'vite-tsconfig-paths'
+import svgr from 'vite-plugin-svgr'
+import { internalIpV4 } from "internal-ip";
+
+const mobile = !!/android|ios/.exec(process.env.TAURI_ENV_PLATFORM);
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  // prevent vite from obscuring rust errors
+export default defineConfig(async () => ({
+  plugins: [react(), viteTsconfigPaths(), svgr()],
+
+  // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
+  //
+  // 1. prevent vite from obscuring rust errors
   clearScreen: false,
-  // Tauri expects a fixed port, fail if that port is not available
+  // 2. tauri expects a fixed port, fail if that port is not available
   server: {
+    port: 5173,
     strictPort: true,
+    host: mobile ? "0.0.0.0" : false,
+    hmr: mobile
+      ? {
+          protocol: "ws",
+          host: await internalIpV4(),
+          port: 1421,
+        }
+      : undefined,
     watch: {
       // 3. tell vite to ignore watching `src-tauri`
       ignored: ["**/src-tauri/**"],
-    }
+    },
   },
-  // to access the Tauri environment variables set by the CLI with information about the current target
-  envPrefix: [
-    'VITE_',
-    'TAURI_PLATFORM',
-    'TAURI_ARCH',
-    'TAURI_FAMILY',
-    'TAURI_PLATFORM_VERSION',
-    'TAURI_PLATFORM_TYPE',
-    'TAURI_DEBUG'
-  ],
-  plugins: [viteTsconfigPaths(), svgr(), react()],
-  build: {
-    // Tauri uses Chromium on Windows and WebKit on macOS and Linux
-    target: process.env.TAURI_PLATFORM == 'windows' ? 'chrome105' : 'safari13',
-    // don't minify for debug builds
-    minify: !process.env.TAURI_DEBUG ? 'esbuild' : false,
-    // produce sourcemaps for debug builds
-    sourcemap: !!process.env.TAURI_DEBUG
-  }
-})
+}));
